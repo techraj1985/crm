690dfc9e61d7b1e693499669a6e633460160c249
Neha Dhiman <neha@geeshasolutions.com>
2026-08-27 08:56:51 -0400

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
    
 <div class="page-content">
                     <div class="container-fluid">
-                        
                         <!-- start page title -->
                         <div class="row">
                             <div class="col-12">
