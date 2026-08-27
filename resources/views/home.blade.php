69f1d08acb867b2d1d2074c65826ec1e34c526f8
Neha Dhiman <neha@geeshasolutions.com>
2026-08-27 09:18:34 -0400

    push all code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
    
 <div class="page-content">
                     <div class="container-fluid">
-                        <!-- start page title -->
+                       
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
